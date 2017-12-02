44567cd9590df98c983807c29f747817482fede1
ScottWorks <23440493+ScottWorks@users.noreply.github.com>
2017-12-01 23:32:25 -0500

    completed CC4

--- Udemy/Complete-JavaScript-Course/Coding-Challenges/CC4.js
+++ Udemy/Complete-JavaScript-Course/Coding-Challenges/CC4.js
@@ -1,45 +1,85 @@
 
-var Question = function (question, userAnswer, correctAnswer) {
-    this.question = question,
-    this.userAnswer = userAnswer,
-    this.correctAnswer = correctAnswer
-}
 
+var Module = (function(){
 
-Question.prototype.displayQA = (function() {
-    Question.prototype.questionNum = Math.floor(Math.random() * this.question.length);
-    console.log(this.question[this.questionNum]);
-
-    var userAnswerArrElem = this.userAnswer[this.questionNum].length;
-    for (var i = 0; i < userAnswerArrElem; i++) {
-        currentElem = this.userAnswer[this.questionNum][i];
-        console.log((i + 1) + ': ' + currentElem);
-    }
-    practiceQuiz.checkAnswer();
-});
-
-
-Question.prototype.checkAnswer = (function() {
-    userAnswer = prompt('Please enter your answer choice...');   
-    console.log(this.correctAnswer[this.questionNum]);   
-    if (userAnswer == this.correctAnswer[this.questionNum]) {
-        console.log('Correct Answer!');
+    var Question = function(question, answer, correctAnswer){
+        this.question = question,
+        this.answer = answer,
+        this.correctAnswer = correctAnswer
     }
 
-    else {
-        console.log('Incorrect Answer!');
+    var Q1 = new Question('What was the first cryptocurrency named?', ['Ethereum Classic', 'Bitcoin', 'Bitcoin Cash', 'Ethereum'], 2);
+
+    var Q2 = new Question('What is the pseudo-name of the Bitcoin whitepaper Author?', ['Vinny Gupta', 'Vitalik Buterin', 'Satoshi Nakamoto', 'Gavin Wood'], 3);
+
+    var Q3 = new Question('What is the underlying technology of Bitcoin, Ethereum, and Dash called?', ['Hashcode', 'Tangle', 'Directed Acylic Graph', 'Blockchain'], 4);
+
+
+    Question.prototype.displayQA = function(){
+        console.log(this.question);
+
+        var answerArrElem = this.answer.length;
+        for (var i = 0; i < answerArrElem; i++) {
+            currentElem = this.answer[i];
+            console.log((i + 1) + ': ' + currentElem);
+        }
     }
-});
+
+    Question.prototype.checkAnswer = function(response, callBack) {        
+        console.log(response);   
+        var score;
+
+        if (response == this.correctAnswer) {
+            console.log('Correct Answer!');
+            score = callBack(true);
+        }
+    
+        else {
+            console.log('Incorrect Answer!');
+            score = callBack(false);                
+        }
+        this.getScore(score);
+    }
+
+    Question.prototype.getScore = function(score){
+        console.log('Current Score: ' + score);
+        console.log('------------------------------------------------------------------------');
+        console.log('------------------------------------------------------------------------');
+        console.log('------------------------------------------------------------------------');
+    }
+
+    function scoreCount(){
+        var score = 0;
+        return function(correct){
+            if(correct){
+                score++;
+            }
+            return score; 
+        }
+    }
+
+    var points = scoreCount();
 
 
-var questionArr = ['What was the first cryptocurrency named?', 'What is the pseudo-name of the Bitcoin whitepaper Author?', 'What is the underlying technology of Bitcoin, Ethereum, and Dash called?'];
+    function getQuestion(){
+        var questions = [Q1, Q2, Q3];
+        
+        var questionNum = Math.floor(Math.random() * questions.length); 
+        
+        questions[questionNum].displayQA();
 
-var answerArr = [['Ethereum Classic', 'Bitcoin', 'Bitcoin Cash', 'Ethereum'], ['Vinny Gupta', 'Vitalik Buterin', 'Satoshi Nakamoto', 'Gavin Wood'], ['Hashcode', 'Tangle', 'Directed Acylic Graph', 'Blockchain']]
+        userResponse = prompt('Please enter your answer choice...');
 
-var correctAnsArr = [2, 3, 4];
+        if(userResponse != 'exit'){
+            questions[questionNum].checkAnswer(userResponse, points) 
+            getQuestion();            
+        } 
+    }
 
-var practiceQuiz = new Question(questionArr, answerArr, correctAnsArr);
+    return{
+        getQuestion: getQuestion
+    }
 
-practiceQuiz.displayQA(); 
+})();
 
-// userAnswer = prompt('Please enter your answer choice...');
+Module.getQuestion();
